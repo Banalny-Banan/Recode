2114b567350789a2230306e0fe1ed6e171cf87c3
Banalny-Banan <elisey1324@gmail.com>
2026-02-14 04:20:07 +0200

    move queie item into a separate file

---
+++ Recode/Views/QueueItemView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Recode.Views;
+
+public partial class QueueItemView : UserControl
+{
+    public QueueItemView()
+    {
+        InitializeComponent();
+    }
+}
